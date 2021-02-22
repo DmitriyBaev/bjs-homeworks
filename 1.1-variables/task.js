02b4c560df8753da3e42f35a0c8068bef5988c13
DmitriyBaev <100maverick100@gmail.com>
2021-02-22 16:33:20 +0300

    homeWork #1

--- 1.1-variables/task.js
+++ 1.1-variables/task.js
@@ -7,21 +7,24 @@
 
 function averageMark(a, g, p) {
 	
-    // реализуйте здесь задание №1
+    let algebra = a, geography = g, physics = p;
+	let averageScore = (algebra + geography + physics) / 3;
     
-	return // ваш ответ;
+	return averageScore;
 }
 
 function sayHello(userName) {
 	
-    // реализуйте здесь задание №2
+    let myName = userName;
+	let message = `Привет, мир! Меня зовут ${myName}.`;
     
-	return // ваш ответ;
+	return message;
 }
 
 function calculateFormula() {
     
-	// реализуйте здесь задание №3
+	let x = 2, y = 22, z = 0;
+	let result = x * y + 5 * z + x - 1;
     
-    return // ваш ответ;
+    return result;
 }
