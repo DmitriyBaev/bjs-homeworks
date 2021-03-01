9609da345399e5a363a0dc1c6e503996f9208e75
DmitriyBaev <100maverick100@gmail.com>
2021-03-01 21:44:42 +0300

    homeWork 01.03.21

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,9 +1,63 @@
+"use strict";
+
+// Задача №1
 function calculateTotalMortgage(percent, contribution, amount, date) {
-    // код для задачи №1 писать здесь
-    // return totalAmount;
+
+  // Попытка сделать проверку на тип данных
+  // percent = +percent;
+  // contribution = +contribution;
+  // amount = +amount;
+
+  // let array = [percent, contribution, amount];
+  // for (let i = 0; i < array.length; i++) {
+  //   if (typeof +array[i] !== "number") {
+  //     return `Параметр ${array.slice([i],[i + 1])} содержит неправильное значение ${array[i]}`;
+  //   }
+  // }
+
+  let creditBody = amount - contribution; // тело кредита
+  console.log(`creditBody ${creditBody}`);
+
+  let years = date.getFullYear() - new Date().getFullYear();
+  let months = date.getMonth() - new Date().getMonth();
+  let quantityMonths = years * 12 + months; // количество месяцев
+  console.log(`months ${quantityMonths}`);
+
+  let monthlyFee =
+    creditBody *
+    (percent / 100 / 12 +
+      percent / 100 / 12 / ((1 + percent / 100 / 12) ** quantityMonths - 1)); // ежемесячная оплата по кредиту
+  console.log(`monthlyFee ${monthlyFee}`);
+
+  let totalAmount = quantityMonths * monthlyFee;
+  console.log(totalAmount.toFixed(2));
+  console.log(typeof totalAmount);
+
+  return +totalAmount.toFixed(2);
 }
 
+// Задача №2
+
+// Вариант 1
 function getGreeting(name) {
-    // код для задачи №2 писать здесь
-    // return greeting;
+  let greeting;
+  if (!name) {
+    name = "Аноним";
+  }
+  greeting = `Привет, мир! Меня зовут ${name}.`;
+
+  return greeting;
 }
+
+// Вариант 2
+// Изначально написал так, но потом подумал,что под условие задачи больше подходит первый вариант
+// function getGreeting(name) {
+//   let greeting;
+//   if (name) {
+//     greeting = `Привет, мир! Меня зовут ${name}.`;
+//   } else {
+//     greeting = `Привет, мир! Меня зовут Аноним.`;
+//   }
+
+//   return greeting;
+// }
